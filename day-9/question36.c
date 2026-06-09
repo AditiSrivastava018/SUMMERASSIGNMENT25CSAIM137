b91115bf38d72fd8e5265bbc3a5d4ff0d09bb7be
Aditi Srivastava <gunjananand144@gmail.com>
2026-06-09 21:29:41 +0530

    Added day-9 folder

---
+++ day-9/question36.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter size of square: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            // Print star on border, space inside
+            if(i == 1 || i == n || j == 1 || j == n) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
