6c598577df8927f4f28b5a5d56360d1e3382fd5c
Aditi Srivastava <gunjananand144@gmail.com>
2026-06-13 15:43:01 +0530

    Added day-13 folder

---
+++ day-13/question50.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,sum=0;
+    float avg;
+    printf("enter no. of elements:");
+    scanf("%d",&n);
+    printf("enter %d elements:\n",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum+= arr[i];
+    }
+    avg= (float)sum/n;
+    printf("sum = %d\n",sum);
+    printf("average = %.2f\n",avg);
+    return 0;
+}
