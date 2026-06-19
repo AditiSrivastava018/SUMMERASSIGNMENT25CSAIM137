eb214a13448cc2e32e8c7e0970942b80548a68e6
Aditi Srivastava <gunjananand144@gmail.com>
2026-06-19 19:06:31 +0530

    Added day-19 folder

---
+++ day-19/question75.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c], transpose[c][r];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Finding transpose
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose Matrix:\n");
+    for(int i = 0; i < c; i++) {
+        for(int j = 0; j < r; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
