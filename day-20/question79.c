795a71b367b58d424ea05df642ae96d6290831ba
Aditi Srivastava <gunjananand144@gmail.com>
2026-06-20 21:21:50 +0530

    Added day-20 folder

---
+++ day-20/question79.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Row-wise sums are:\n");
+
+    for(int i = 0; i < r; i++) {
+        int sum = 0;
+
+        for(int j = 0; j < c; j++) {
+            sum += a[i][j];
+        }
+
+        printf("Sum of Row %d = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
